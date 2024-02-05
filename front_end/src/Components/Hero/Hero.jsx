274a5c4667e1e83e0b18da5198d5f74c8fe36f58
Remus92 <remusbalaj@yahoo.com>
2024-02-05 13:59:04 +0200

    Users data and cart items linked to mongoDB database. New collections logic added. Admin panel completed. 05.02.2024

--- front_end/src/Components/Hero/Hero.jsx
+++ front_end/src/Components/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Hero.css';
 import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/hero_image.png'
+import hero_image from '../Assets/hero2_image.png'
 import shirt_icon from '../Assets/shirt_icon.png'
 
 const Hero = () => {
-  return (
-    <div className='hero'>
+   return (
+   <div className='hero'>
       <div className="hero-left">
          <div>
          <h2>New Arrivals</h2>
@@ -24,8 +24,8 @@ const Hero = () => {
       <div className="hero-right">
          <img src={hero_image} alt="" />
       </div>
-    </div>
-  )
+   </div>
+   )
 }
 
 export default Hero
--- front_end/src/Pages/LoginSignup.jsx
+++ front_end/src/Pages/LoginSignup.jsx
@@ -1,18 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
+
+   const [state,setState] = useState("Login");
+   const [formData,setFormData] = useState({
+      username:"",
+      password:"",
+      email:""
+   })
+
+   const changeHandler = (e) =>{
+      setFormData({...formData,[e.target.name]:e.target.value})
+   }
+
+   const login = async () =>{
+      console.log("Login function executed",formData);
+      let responseData;
+      await fetch('http://localhost:4000/login',{
+         method:'POST',
+         headers:{
+            Accept:'application/form-data',
+            'Content-Type':'application/json',
+         },
+         body:JSON.stringify(formData),
+      }).then((response)=>response.json()).then((data)=>responseData=data)
+      
+      if(responseData.success){
+         localStorage.setItem('auth-token',responseData.token);
+         window.location.replace("/");
+      }
+      else{
+         alert(responseData.errors)
+      }
+   }
+
+   const signup = async () =>{
+      console.log("Sign up function executed",formData);
+      let responseData;
+      await fetch('http://localhost:4000/signup',{
+         method:'POST',
+         headers:{
+            Accept:'application/form-data',
+            'Content-Type':'application/json',
+         },
+         body:JSON.stringify(formData),
+      }).then((response)=>response.json()).then((data)=>responseData=data)
+      
+      if(responseData.success){
+         localStorage.setItem('auth-token',responseData.token);
+         window.location.replace("/");
+      }
+      else{
+         alert(responseData.errors)
+      }
+   }
+
+
    return (
       <div className="loginsignup">
          <div className="loginsignup-container">
-            <h1>Sign Up</h1>
+            <h1>{state}</h1>
             <div className="loginsignup-fields">
-               <input type="text" placeholder="Your name" />
-               <input type="email" placeholder="Your email address" />
-               <input type="password" placeholder="Your password"/>
+               {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder="Your username" />:<></>}
+               <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder="Your email address" />
+               <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder="Your password" />
             </div>
-            <button>Continue</button>
-            <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+            <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+            {state==="Sign Up"?
+            <p className="loginsignup-login">Already have an account? <span onClick={()=>{setState("Login")}}>Login here.</span></p>:
+            <p className="loginsignup-login">To create a new account <span onClick={()=>{setState("Sign Up")}}>click here.</span></p>
+            }
+            
             <div className="loginsignup-agree">
                <input type="checkbox" name='' id=''/>
                <p>By continuing, I agree to the terms of use & privacy policy.</p>
